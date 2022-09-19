fb235327b858841b8d01ca5c3860ca8c35d3ac9f
zidouto2 <aryazid10@gmail.com>
2022-09-20 01:45:37 +0700

    Penghapusan daerah

---
+++ Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan</h1>";
+        echo "Nama saya Muhammad Aryazid Saya tinggal di Pamulang pelajaran yang saya sukai adalah Matematika";
+    }
+}
