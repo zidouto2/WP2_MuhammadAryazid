54376ac2cfcf972c08b849198f72b9bfd080e52b
zidouto2 <113604360+zidouto2@users.noreply.github.com>
2022-09-29 11:35:44 +0700

    Add files via upload

---
+++ view-latihan.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <title> Latihan1 </title>
+</head>
+
+<body>
+    YuK kita belajar Web Programming!<br>
+    Nilai1 = <?= $nilai1; ?>
+    Nilai2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 ."+". $nilai2 ."=".$hasil; ?>
+</body>
+</html>
