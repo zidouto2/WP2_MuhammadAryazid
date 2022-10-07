b55ec8f181620a5ed0e74e0503b122560bdba79b
zidouto2 <113604360+zidouto2@users.noreply.github.com>
2022-10-07 08:59:47 +0700

    Pertemuan 4

---
+++ v_header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Web Prog II | Merancang template sederhana </title>
+    <link rel="stylesheet" type="text/css" href="http://localhost/booking/assets/css/stylebuku.css">
+</head>
+<body>
+    <div id="wrapper">
+    <header>
+        <hgroup>
+            <h1> RentalBuku.Net By Aryazid </h1>
+            <h3> Membuat template sederhana dengan CodeIgniter </h3>
+        </hgroup>
+        <nav>
+            <ul>
+            <li><a href="http://localhost/booking/index.php/Web">Home</a></li>
+            <li><a href="http://localhost/booking/index.php/Web/about">About</a></li>
+            </ul>
+        </nav>
+        <div class="clear"></div>
+    </header>
